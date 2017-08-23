0acd72844dc9005ccb1dc20626b65e0759250860
Vitaliy Trush <vitaliy.trush@playtech.com>
2017-08-23 16:55:28 +0300

    Added insertion & selection sort algoritm

--- algoritm.js
+++ algoritm.js
@@ -2,12 +2,10 @@ Array.prototype.linearIndexOf = function(val) {
     var count = 0;
     for (var i = 0; i < this.length; i++) {
         if (this[i] === val) {
-            console.log('count: ' + count);
             return i;
         }
         count++;
     }
-    console.log('count: ' + count);
 
     return -1;
 };
@@ -21,7 +19,6 @@ Array.prototype.binaryIndexOf = function(val) {
         var mid = Math.floor((lo + hi) / 2);
 
         if (this[mid] === val) {
-            console.log('count: ' + count);
             return mid;
         } else if (this[mid] > val) {
             hi = mid - 1;
@@ -43,7 +40,6 @@ Array.prototype.interpolationIndexOf = function(val) {
         var mid = lo + Math.floor((hi - lo) * (val - this[lo]) / (this[hi] - this[lo]));
 
         if (this[mid] === val) {
-            console.log('count: ' + count);
             return mid;
         } else if (this[mid] > val) {
             hi = mid - 1;
@@ -52,6 +48,54 @@ Array.prototype.interpolationIndexOf = function(val) {
         }
         count++;
     }
-    console.log(count);
+    
     return -1;
+};
+
+Array.prototype.bubbleSort = function() {
+    var arr = this.slice(0);
+
+    function swap(i, j) {
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    for(var i = 1; i < arr.length; i++) {
+        for(var j = 0; j < arr.length - i; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(j, j+1);
+            }
+        }
+    }
+    return arr;
+};
+
+Array.prototype.insertionSort = function() {
+    var arr = this.slice(0);
+
+    function swap(i, j) {
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    for(var i = 1; i < arr.length; i++) {
+        for(var j = i; j > 0 && arr[j-1] > arr[j]; j--) {
+            swap(j-1, j);
+        }
+    }
+    return arr;
+};
+
+Array.prototype.selectionSort = function() {
+    var arr = this.slice(0);
+
+    function swap(i, j) {
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+
 };
